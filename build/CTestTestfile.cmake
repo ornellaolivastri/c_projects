e11fbd6d46b05b378ca1ab154e03cd48ddfb0952
ornella olivastri <oolivastri@frba.utn.edu.ar>
2021-11-23 19:44:28 -0300

    reorganization of files and second makefile test

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Usuario/Orne/Desktop/proyectos/c_projects
-# Build directory: D:/Usuario/Orne/Desktop/proyectos/c_projects/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
